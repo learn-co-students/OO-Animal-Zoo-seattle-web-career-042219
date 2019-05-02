7e9199d02be5833608190f3c3043bebbc047dcd2
Hanna Engel <hengel@alumni.berklee.edu>
2019-05-02 11:24:29 -0700

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,22 @@
-class Animal
 
+class Animal
+attr_accessor :weight, :zoo
+attr_reader :nickname, :species
+@@all = []
+
+def self.all
+@@all
+end
+
+def self.find_by_species(species)
+@@all.find_all{|animal|animal.species==species}
+end
+
+
+def initialize(species, weight, nickname)
+@species = species
+@weight = weight
+@nickname = nickname
+@@all<< self
+end
 end
