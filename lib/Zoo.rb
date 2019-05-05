91d69e6938b78a5dfb0f09df48dc46391ac0497f
github username <github email address>
2019-05-05 13:37:41 -0700

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,70 @@
+
+
 class Zoo
 
+  @@all = []
+
+  attr_accessor :name, :location
+
+  def initialize(name, location)
+    @name = name
+    @location = location
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def animals
+    Animal.all.select do |animal_obj|
+      animal_obj.zoo == self
+    end
+    #should return all the animals that a specific instance of a zoo has.
+  end
+
+  def animal_species
+    species_ary = []
+    Animal.all.each do |animal_obj|
+      if animal_obj.zoo == self
+        species_ary << animal_obj.species
+      end
+    end
+    species_ary.uniq
+    #should return an array of all the species (as strings) of the
+    #animals in the zoo. However, if you have two dogs, it should only return one "Dog"
+    #string (aka an unique array).
+  end
+
+  def find_by_species(species)
+    Animal.all.select do |animal_obj|
+      animal_obj.zoo == self && animal_obj.species == species
+    end
+    #should take in an animal's species as an argument and return an array
+    #of all the animals in that zoo, which are of that species.
+  end
+
+  def animal_nicknames
+    an_ary = []
+    Animal.all.each do |animal_obj|
+      if animal_obj.zoo == self
+        an_ary << animal_obj.nickname
+      end
+    end
+    an_ary
+    #should return an array of all the nicknames of animals that a specific
+    #instance of a zoo has.
 end
+
+  def self.find_by_location(location)
+    @@all.select do |zoo_obj|
+      zoo_obj.location == location
+    end
+    #should take in a location as an argument and return an array of all the zoos
+    #within that location.
+  end
+
+end
+
+
+###############################
--- run.rb
+++ run.rb
@@ -5,6 +5,36 @@ require 'pry'
 
 #Test your code here
 
+#zoo objects
+sf_zoo = Zoo.new("San Fransisco Zoo", "San Fransisco")
+woodland_park_zoo = Zoo.new("Woodland Park Zoo", "Seattle")
+san_diego_zoo = Zoo.new("San Diego Zoo", "San Diego")
+happy_hollow_zoo = Zoo.new("Happy Hollow Zoo", "San Jose")
+private_zoo = Zoo.new("My Private Zoo", "San Jose")
+
+#animal objects
+lemur = Animal.new("lemur", 29, "Cutie", sf_zoo)
+lemur2 = Animal.new("lemur", 28, "Cutie Pie", sf_zoo)
+tiger = Animal.new("tiger", 450, "Stripes", sf_zoo)
+elephant = Animal.new("elephant", 12000, "Wrinkles", woodland_park_zoo)
+lion = Animal.new("lion", 500, "Roary", woodland_park_zoo)
+bear = Animal.new("bear", 350, "Fuzzy", san_diego_zoo)
+zebra = Animal.new("zebra", 350, "Oreo", san_diego_zoo)
+goat = Animal.new("goat", 25, "Hungry", happy_hollow_zoo)
+meerkat = Animal.new("meerkat", 7, "Sneaky", happy_hollow_zoo)
+meerkat2 = Animal.new("meerkat", 7, "Alarmist", happy_hollow_zoo)
+
+#other zoo methods
+all_zoos = Zoo.all
+animals_at_zoo = happy_hollow_zoo.animals
+all_species_at_zoo = sf_zoo.animal_species
+all_animals_of_species = sf_zoo.find_by_species("lemur")
+nicknames = happy_hollow_zoo.animal_nicknames
+by_location = Zoo.find_by_location("San Jose")
+
+#other animal methods
+all_animals = Animal.all
+find_species = Animal.find_by_species("meerkat")
 
 binding.pry
 puts "done"
