7510ebd5cc4a9c9f0e53e2898a7c2f506cd747ce
github username <github email address>
2019-05-05 20:49:02 -0700

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,43 @@
 class Zoo
+	attr_accessor :name, :location
 
+	@@all = []
+
+	def initialize(name, location)
+		@name = name
+		@location = location
+		@@all << self
+	end
+
+	def animals
+		Animal.all.select do |animal|
+			animal.zoo == self
+		end
+	end
+
+	def animal_species
+		self.animals.map do |animal|
+			animal.species
+		end.uniq
+	end
+
+	def find_by_species(species)
+		Animal.find_by_species(species)
+	end
+
+	def animal_nicknames
+		self.animals.map do |animal|
+			animal.nickname
+		end
+	end
+
+	def self.find_by_location(location)
+		self.all.select do |zoo|
+			zoo.location == location
+		end
+	end
+
+	def self.all
+		@@all
+	end
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,31 @@ require 'pry'
 
 
 #Test your code here
+anim1 = Animal.new("Tiger", 300, "Terry")
+anim2 = Animal.new("Bear", 500, "Berry")
+anim3 = Animal.new("Rabbit", 800, "Harry")
+anim4 = Animal.new("Bear", 600, "Ferry")
 
+zoo1 = Zoo.new("Fair Zoo", "NY")
+zoo2 = Zoo.new("Loopin Zoo", "PA")
+zoo3 = Zoo.new("Trailburn Animal Collective", "NY")
+
+anim1.zoo = zoo1
+anim2.zoo = zoo2
+anim3.zoo = zoo3
+anim4.zoo = zoo2
+
+Zoo.all
+
+zoo1.animals
+zoo2.animal_species
+zoo2.find_by_species("Bear")
+zoo3.animal_nicknames
+Zoo.find_by_location("NY")
+
+Animal.all
+anim3.zoo
+Animal.find_by_species("Bear")
 
 binding.pry
 puts "done"
